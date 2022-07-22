0315141840be58ef9168578a8c838c2d0a17cf41
svkenier <svkenier@gmail.com>
2022-07-22 01:39:40 -0400

    add de ejercicios dia 11

---
+++ dia11/index.js
@@ -0,0 +1,10 @@
+// Escribir una función que tome un objeto con dos propiedades como argumento
+// Debe devolver el valor de la propiedad con clave país
+
+const showCountry = (obj) => {
+  return obj.country;
+};
+console.log(toShowCountry({ continent: "Asia", country: "Japon" }));
+
+//comentario 1: funcion muy simple que retorna la propieda de el objeto con
+//codigo ciudada
---
+++ dia11/index2.js
@@ -0,0 +1,12 @@
+// Escribir una función que tome una cadena como argumento
+// Crear un objeto que tenga una propiedad con clave 'clave'
+//y un valor igual a la cadena
+// Devolver el objeto
+
+const objectWithParameterProperty = (a) => {
+  return { key: a };
+};
+
+console.log(objectWithParameterProperty("33"));
+
+//comentario 2: retone un objeto con el valor igual parametro de la funcion
